d5955cd769d58195f873d0a8e2b3c3ecf2196280
nyanpoketto-kujira <chievoverony5@gmail.com>
2026-05-05 05:29:57 +0700

    fix: move lazy to leader-lz and use leader-l for laravel

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Pindahin menu Lazy biar nggak bentrok sama Laravel
+vim.keymap.del("n", "<leader>l")
+vim.keymap.set("n", "<leader>lz", "<cmd>Lazy<cr>", { desc = "Lazy" })
